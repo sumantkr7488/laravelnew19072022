f878637482fb8fb960911fe109a6400fc87d3732
Eding Muhamad <masaruedo@yahoo.com>
2019-11-21 00:31:15 +0700

    implement one to many relationship

--- app/Company.php
+++ app/Company.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $fillable = ['name', 'address', 'email', 'website'];
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
 
 /*
--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
