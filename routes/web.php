5836f7b834b46808ff243c3c9c643471715f448e
Eding Muhamad <masaruedo@yahoo.com>
2019-11-22 00:30:35 +0700

    define some routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function () {
+    return "<h1>All contacts</h1>";
+})->name('contacts.index');
+
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
+
+Route::get('/contacts/{id}', function ($id) {
+    return App\Contact::find($id);
+})->name('contacts.show');
