38db78fc7c7fcc6a7dfd023a931f07b856309481
Eding Muhamad <masaruedo@yahoo.com>
2019-11-24 00:31:55 +0700

    display all companies on dropdown

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Contact;
+use App\Company;
 
 class ContactController extends Controller
 {
     public function index()
     {
+        $companies = Company::orderBy('name')->pluck('name', 'id');
         $contacts = Contact::orderBy('first_name', 'asc')->paginate(10);
 
-        return view('contacts.index', compact('contacts'));
+        return view('contacts.index', compact('contacts', 'companies'));
     }
 
     public function create()
