8b3beac0b489ad8ee4882a690d5c09c3243c765c
Eding Muhamad <masaruedo@yahoo.com>
2019-11-25 21:47:24 +0700

    validate and persist data on store method

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,7 +11,7 @@ class ContactController extends Controller
     public function index()
     {
         $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
-        $contacts = Contact::orderBy('first_name', 'asc')->where(function ($query) {
+        $contacts = Contact::orderBy('id', 'desc')->where(function ($query) {
             if ($companyId = request('company_id')) {
                 $query->where('company_id', $companyId);
             }
@@ -29,7 +29,17 @@ class ContactController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->except('first_name', 'last_name'));
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'company_id' => 'required|exists:companies,id',
+        ]);
+
+        Contact::create($request->all());
+
+        return redirect()->route('contacts.index')->with('message', "Contact has been added successfully");
     }
 
     public function show($id)
