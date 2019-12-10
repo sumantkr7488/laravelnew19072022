18af085672f1a68cb0b2c2fd7977d057386c3fda
Eding Muhamad <masaruedo@yahoo.com>
2019-12-10 11:22:27 +0700

    Implement global scopes

--- app/Contact.php
+++ app/Contact.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Scopes\FilterScope;
+use App\Scopes\SearchScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
@@ -17,17 +19,12 @@ class Contact extends Model
     {
         return $query->orderBy('id', 'desc');
     }
-    
-    public function scopeFilter($query)
+
+    protected static function boot()
     {
-        if ($companyId = request('company_id')) {
-            $query->where('company_id', $companyId);
-        }
+        parent::boot();
 
-        if ($search = request('search')) {
-            $query->where('first_name', 'LIKE', "%{$search}%");
-        }
-
-        return $query;
+        static::addGlobalScope(new FilterScope);
+        static::addGlobalScope(new SearchScope);
     }
 }
