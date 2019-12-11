abe1365a5730b40476429b40cc48f6a07415f109
Eding Muhamad <masaruedo@yahoo.com>
2019-12-11 14:17:25 +0700

    make the global scopes reusable

--- app/Contact.php
+++ app/Contact.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use App\Scopes\FilterScope;
-use App\Scopes\SearchScope;
+use App\Scopes\ContactSearchScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'company_id'];
+
+    public $filterColumns = ['company_id'];
 
     public function company()
     {
@@ -25,6 +27,6 @@ class Contact extends Model
         parent::boot();
 
         static::addGlobalScope(new FilterScope);
-        static::addGlobalScope(new SearchScope);
+        static::addGlobalScope(new ContactSearchScope);
     }
 }
--- app/Scopes/SearchScope.php
+++ app/Scopes/SearchScope.php
@@ -8,15 +8,34 @@ use Illuminate\Database\Eloquent\Builder;
 
 class SearchScope implements Scope
 {
+    protected $searchColumns = [];
+    
     public function apply(Builder $builder, Model $model)
     {
-        if ($search = request('search')) {
-            $builder->where('first_name', 'LIKE', "%{$search}%");
-            $builder->orWhere('last_name', 'LIKE', "%{$search}%");
-            $builder->orWhere('email', 'LIKE', "%{$search}%");
-            $builder->orWhereHas('company', function ($query) use ($search) {
-                $query->where('name', 'LIKE', "%{$search}%");
-            });
+        if ($search = request('search')) 
+        {
+            $columns = property_exists($model, 'searchColumns') ? $model->searchColumns : $this->searchColumns;
+            
+            foreach ($columns as $index => $column) 
+            {
+                $arr = explode('.', $column);
+                $method = $index === 0 ? "where" : "orWhere";
+
+                if (count($arr) == 2) 
+                {
+                    $method .= "Has";
+
+                    list($relationship, $col) = $arr;
+                    
+                    $builder->$method($relationship, function ($query) use ($search, $col) {
+                        $query->where($col, 'LIKE', "%{$search}%");
+                    });    
+                }
+                else {
+                    $builder->$method($column, 'LIKE', "%{$search}%");
+                }
+            }
+
         }
     }
 }
