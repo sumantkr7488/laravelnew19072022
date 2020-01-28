8ff6acdb675df205a8b7e4519db9a5b0507be6ce
Eding Muhamad <masaruedo@yahoo.com>
2020-01-29 05:09:28 +0700

    using resourceful route for contact and company resources

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', 'ContactController@index')->name('contacts.index');
+// Route::get('/contacts', 'ContactController@index')->name('contacts.index');
 
-Route::post('/contacts', 'ContactController@store')->name('contacts.store');
+// Route::post('/contacts', 'ContactController@store')->name('contacts.store');
 
-Route::get('/contacts/create', 'ContactController@create')->name('contacts.create');
+// Route::get('/contacts/create', 'ContactController@create')->name('contacts.create');
 
-Route::get('/contacts/{contact}', 'ContactController@show')->name('contacts.show');
+// Route::get('/contacts/{contact}', 'ContactController@show')->name('contacts.show');
 
-Route::put('/contacts/{contact}', 'ContactController@update')->name('contacts.update');
+// Route::put('/contacts/{contact}', 'ContactController@update')->name('contacts.update');
 
-Route::delete('/contacts/{contact}', 'ContactController@destroy')->name('contacts.destroy');
+// Route::delete('/contacts/{contact}', 'ContactController@destroy')->name('contacts.destroy');
 
-Route::get('/contacts/{contact}/edit', 'ContactController@edit')->name('contacts.edit');
+// Route::get('/contacts/{contact}/edit', 'ContactController@edit')->name('contacts.edit');
+// Route::resource('/contacts', 'ContactController');
+// Route::resource('/contacts', 'ContactController')->only(['create', 'store', 'edit', 'update', 'destroy']);
+// Route::resource('/contacts', 'ContactController')->except(['index', 'show']);
+Route::resources([
+    '/contacts' => 'ContactController',
+    '/companies' => 'CompanyController',
+]);
 
 Auth::routes(['verify' => true]);
 
